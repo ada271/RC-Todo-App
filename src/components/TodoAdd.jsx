a1554219430504caf3792a9c876017e1210422dd
ada <h.kaansolmaz@gmail.com>
2024-10-07 15:58:28 +0300

    close/show

--- src/components/TodoAdd.jsx
+++ src/components/TodoAdd.jsx
@@ -1,28 +1,33 @@
 import React, { useState } from "react";
 
 const TodoAdd = ({ doing, setDoing }) => {
-
     const [button, setButton] = useState(true);
 
     return (
         <div>
             <header>
                 <h1>TO DO APP</h1>
-                <button onClick={()=>setButton(!button)} className="btn" style={{ backgroundColor: button ? "green" : "purple" }}>
-                    CLOSE ADD TASK BAR
+                <button
+                    onClick={() => setButton(!button)}
+                    className="btn"
+                    style={{ backgroundColor: button ? "green" : "purple" }}
+                >
+                    {button ? "CLOSE" : "SHOW"} ADD TASK BAR
                 </button>
             </header>
-            <form action="">
-                <div className="formControl">
-                    <label htmlFor="text">Task</label>
-                    <input type="text" id="text" />
-                </div>
-                <div className="formControl">
-                    <label htmlFor="date">Day & Time</label>
-                    <input type="datetime-local" id="date" />
-                </div>
-                <button className="btn btn-submit">SUBMIT</button>
-            </form>
+            {button && (
+                <form action="">
+                    <div className="formControl">
+                        <label htmlFor="text">Task</label>
+                        <input type="text" id="text" />
+                    </div>
+                    <div className="formControl">
+                        <label htmlFor="date">Day & Time</label>
+                        <input type="datetime-local" id="date" />
+                    </div>
+                    <button className="btn btn-submit">SUBMIT</button>
+                </form>
+            )}
         </div>
     );
 };
